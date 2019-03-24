151d4d7f4ab50cbf1b2b5bf65669aca9b5f7e9b2
Antonio Felipe Melo Silva <antoniofelipems@gmail.com>
2019-03-23 23:31:44 -0300

    Finaliza exercício e altera array para char

--- Exercicio3.c
+++ Exercicio3.c
@@ -3,12 +3,11 @@
 
 int main()
 {
-    int temps[50];
+    char temps[50];
     int i;
     int aboveAvg = 0;
     for (i = 0; i < 50; i++){
-        int num = (rand() % (100 - (-100) + 1)) + 100;
-        printf("Num %d\n", num);
+        char num = rand() % 101 - 50;        
         temps[i] = num;
     }
 
@@ -18,17 +17,14 @@ int main()
     }
 
     float avg = total / 50;
-    printf("Média das temperaturas: %.6f\n", avg);
+    printf("Média das temperaturas: %.2f\n", avg);
 
     for(i=0; i< 50; i++){
         if(temps[i] > avg){
             aboveAvg += 1;
-            printf("Temperatura: %d\n", temps[i]);            
+            printf("Temperatura acima da média: %d\n", temps[i]);            
         }
     }
 
     printf("Quantidade de elementos acima da média: %d\n", aboveAvg);
-
-
-
 }
